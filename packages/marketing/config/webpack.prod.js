272d618745cf031c80de27041b06713a2b161561
krishnaUIDev <krishna.kondoju007@gmail.com>
2021-01-03 19:54:23 -0500

    added marketing

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publickPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
