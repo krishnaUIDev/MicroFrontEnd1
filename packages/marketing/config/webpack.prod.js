0f52cfb0789736abfa417544e723112ef710ffbe
krishnaUIDev <krishna.kondoju007@gmail.com>
2021-01-03 20:42:34 -0500

    added

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
-//test
+
 const prodConfig = {
   mode: "production",
   output: {
