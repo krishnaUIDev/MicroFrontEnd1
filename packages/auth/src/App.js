58312800a6df24f8f782f1a387201b6a3f76a1d0
krishnaUIDev <krishna.kondoju007@gmail.com>
2021-01-04 23:36:47 -0500

    added

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -4,12 +4,11 @@ import {
   createGenerateClassName,
   StylesProvider,
 } from "@material-ui/core/styles";
-
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: "marketing",
+  productionPrefix: "auth",
 });
 const App = ({ history }) => {
   return (
@@ -17,8 +16,8 @@ const App = ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
+            <Route exact path="/auth/signin" component={SignIn} />
+            <Route exact path="/auth/signup" component={SignUp} />
           </Switch>
         </Router>
       </StylesProvider>
--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,10 +1,20 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathName } = history.location;
+        if (pathName !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
